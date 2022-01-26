7915ba7c3cc5878dffd8545727def4e410c06a62
djn-hgs <daryl.noyce@highgateschool.org.uk>
2022-01-26 20:11:10 +0000

    Functions in main

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+float simpleDiv(float a, float b) {
+  return a / b;
+}
+
+float simpleMult(float a, float b) {
+  return a * b;
+}
+
 int main(void) {
 
-  printf("Hello World");
+  float a = 2;
+  float b = 7;
+
+  float c = simpleDiv(a, b);
+  float d = simpleMult(a, b);
+
+  printf("%f divided by %f is %f\n", a, b , c);
+  printf("%f multiplied by %f is %f\n", a, b , d);
 
   return 0;
 }
